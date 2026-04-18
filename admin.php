b7ea3b2393a20379d26aea2ba3023fc255bbc1b4
Sagar787898 <jasminekidsindia@gmail.com>
2026-04-18 08:38:56 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -48,7 +48,7 @@ if(isset($_GET['status'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>StreamX | Admin</title>
+    <title>Roseflix | Admin</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <style>
